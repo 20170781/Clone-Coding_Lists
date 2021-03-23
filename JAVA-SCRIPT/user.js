7a161f45c0d06cfddb86f2e62b7cda6e88040f6b
20170781 <lkh981107@naver.com>
2021-03-23 23:39:53 +0900

    Study java script with 'JEJU CODING'

---
+++ JAVA-SCRIPT/user.js
@@ -0,0 +1,111 @@
+//JSON Generator로 불러온 user 값들
+var user = [
+  {
+    "_id": "6059f9bdf2c49271641c7dfd",
+    "age": 29,
+    "eyeColor": "green",
+    "name": "Bettie Mcgowan",
+    "gender": "female",
+    "email": "bettiemcgowan@signity.com",
+    "phone": "+1 (946) 466-3835"
+  },
+  {
+    "_id": "6059f9bd97af6e8030b27feb",
+    "age": 31,
+    "eyeColor": "blue",
+    "name": "Nona Miranda",
+    "gender": "female",
+    "email": "nonamiranda@signity.com",
+    "phone": "+1 (878) 540-3163"
+  },
+  {
+    "_id": "6059f9bd8f82626ba6bd2a25",
+    "age": 30,
+    "eyeColor": "brown",
+    "name": "Zelma Mercer",
+    "gender": "female",
+    "email": "zelmamercer@signity.com",
+    "phone": "+1 (994) 527-3772"
+  },
+  {
+    "_id": "6059f9bdfcb294b01d79b814",
+    "age": 32,
+    "eyeColor": "brown",
+    "name": "Riddle Chaney",
+    "gender": "male",
+    "email": "riddlechaney@signity.com",
+    "phone": "+1 (850) 466-3902"
+  },
+  {
+    "_id": "6059f9bd0c27973a8d469453",
+    "age": 33,
+    "eyeColor": "brown",
+    "name": "Tasha Caldwell",
+    "gender": "female",
+    "email": "tashacaldwell@signity.com",
+    "phone": "+1 (868) 576-2137"
+  },
+  {
+    "_id": "6059f9bdec1bf8b89bc582aa",
+    "age": 23,
+    "eyeColor": "brown",
+    "name": "Denise Briggs",
+    "gender": "female",
+    "email": "denisebriggs@signity.com",
+    "phone": "+1 (976) 560-3298"
+  },
+  {
+    "_id": "6059f9bd65156341814438ef",
+    "age": 24,
+    "eyeColor": "blue",
+    "name": "Porter Mills",
+    "gender": "male",
+    "email": "portermills@signity.com",
+    "phone": "+1 (854) 435-2724"
+  },
+  {
+    "_id": "6059f9bd52ca904d70f0bd4a",
+    "age": 21,
+    "eyeColor": "brown",
+    "name": "Knox Murray",
+    "gender": "male",
+    "email": "knoxmurray@signity.com",
+    "phone": "+1 (973) 582-2833"
+  },
+  {
+    "_id": "6059f9bd0cfd428592cd97f1",
+    "age": 28,
+    "eyeColor": "green",
+    "name": "Rios Kirkland",
+    "gender": "male",
+    "email": "rioskirkland@signity.com",
+    "phone": "+1 (889) 563-2628"
+  },
+  {
+    "_id": "6059f9bd974da39fead24e09",
+    "age": 30,
+    "eyeColor": "blue",
+    "name": "Jolene Fox",
+    "gender": "female",
+    "email": "jolenefox@signity.com",
+    "phone": "+1 (848) 429-3448"
+  },
+  {
+    "_id": "6059f9bd50b61d0323d18454",
+    "age": 34,
+    "eyeColor": "green",
+    "name": "Clemons Gross",
+    "gender": "male",
+    "email": "clemonsgross@signity.com",
+    "phone": "+1 (952) 579-3074"
+  },
+  {
+    "_id": "6059f9bd58910af01ef3639b",
+    "age": 35,
+    "eyeColor": "green",
+    "name": "Lessie Burnett",
+    "gender": "female",
+    "email": "lessieburnett@signity.com",
+    "phone": "+1 (915) 542-2118"
+  }
+]
