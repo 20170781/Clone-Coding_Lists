d5e41f3213e4862c6ab9b8b4009e75d2b8ad9eee
20170781 <lkh981107@naver.com>
2021-06-22 23:30:15 +0900

    remove arguments from paintGreetings()

--- momentum(js)/app.js
+++ momentum(js)/app.js
@@ -6,16 +6,16 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    event.preventDefault();       // prevent browser's basic work
+    // event.preventDefault();       // prevent browser's basic work
     loginForm.classList.add(HIDDEN_CLASSNAME);
 
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreetings();
 }
 
 // when refresh
-function paintGreetings(username) {
+function paintGreetings() {
+    const username = localStorage.getItem(USERNAME_KEY);
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -26,5 +26,5 @@ if(savedUsername === null){
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    paintGreetings(savedUsername);
+    paintGreetings();
 }
