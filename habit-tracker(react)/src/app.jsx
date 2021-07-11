b0b5a6f0c68a487fe49773b78716cc44f1b939cc
20170781 <lkh981107@naver.com>
2021-07-11 23:44:29 +0900

    add react skills

--- habit-tracker(react)/src/app.jsx
+++ habit-tracker(react)/src/app.jsx
@@ -1,8 +1,84 @@
+import React, { Component } from "react";
 import "./app.css";
+import Navbar from "./components/navbar";
 import Habits from "./components/habits";
 
-function App() {
-  return <Habits />;
+class App extends Component {
+  state = {
+    habits_li: [
+      { id: 1, name: "Reading", count: 0 },
+      { id: 2, name: "Running", count: 0 },
+      { id: 3, name: "Coding", count: 0 },
+    ],
+  };
+
+  //state는 직접 변경x, 그저 id 같은지 확인용
+  handleIncrement = (habit) => {
+    const habits_cp = this.state.habits_li.map((item) => {
+      // item: 기존, habit: 새로운
+      if (item.id === habit.id) {
+        return { ...habit, count: habit.count + 1 };
+      }
+      return item;
+    });
+    this.setState({ habits_li: habits_cp });
+  };
+
+  handleDecrement = (habit) => {
+    const habits_cp = this.state.habits_li.map((item) => {
+      if (item.id === habit.id) {
+        const count = habit.count - 1;
+        return { ...habit, count: count < 0 ? 0 : count };
+      }
+      return item;
+    });
+    this.setState({ habits_li: habits_cp });
+  };
+
+  handleDelete = (habit) => {
+    const habits_cp = this.state.habits_li.filter(
+      (item) => item.id !== habit.id
+    );
+    this.setState({ habits_li: habits_cp });
+  };
+
+  handleAdd = (name) => {
+    const habits_cp = [
+      ...this.state.habits_li,
+      { id: Date.now(), name, count: 0 },
+    ];
+    this.setState({ habits_li: habits_cp });
+  };
+
+  handleReset = () => {
+    const habits_cp = this.state.habits_li.map((habit) => {
+      if (habit.count !== 0) {
+        return { ...habit, count: 0 };
+      }
+      return habit;
+    });
+    this.setState({ habits_li: habits_cp });
+  };
+
+  render() {
+    return (
+      <>
+        <Navbar
+          totalCount={
+            this.state.habits_li.filter((item) => item.count > 0).length
+          }
+        />
+        <Habits
+          habits_li={this.state.habits_li}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+          onAdd={this.handleAdd}
+          onReset={this.handleReset}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
--- habit-tracker(react)/src/components/habits.jsx
+++ habit-tracker(react)/src/components/habits.jsx
@@ -1,51 +1,27 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import Habit from "./habit";
+import HabitAddForm from "./habitAddForm";
 
-class Habits extends Component {
-  state = {
-    habits_li: [
-      { id: 1, name: "Reading", count: 0 },
-      { id: 2, name: "Running", count: 0 },
-      { id: 3, name: "Coding", count: 0 },
-    ],
-  };
-
-  //argument habit은 habit.jsx에서 this.props.habit이다.
-  //habits의 handle함수와 habit의 handle함수는 다르다.
-  handleIncrement = (habit) => {
-    const habits_cp = [...this.state.habits_li];
-    const index = habits_cp.indexOf(habit);
-    habits_cp[index].count++;
-    this.setState({ habits_li: habits_cp });
-  };
-  handleDecrement = (habit) => {
-    const habits_cp = [...this.state.habits_li];
-    const index = habits_cp.indexOf(habit);
-    const count = habits_cp[index].count - 1;
-    habits_cp[index].count = count < 0 ? 0 : count;
-    this.setState({ habits_li: habits_cp });
-  };
-  handleDelete = (habit) => {
-    const habits_cp = this.state.habits_li.filter(
-      (item) => item.id !== habit.id
-    );
-    console.log(habits_cp);
-    this.setState({ habits_li: habits_cp });
-  };
-
+class Habits extends PureComponent {
   render() {
     return (
-      <ul>
-        {this.state.habits_li.map((habit) => (
-          <Habit
-            key={habit.id}
-            habit={habit}
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-            onDelete={this.handleDelete}
-          />
-        ))}
-      </ul>
+      <>
+        <HabitAddForm onAdd={this.props.onAdd} />
+        <ul className="habits">
+          {this.props.habits_li.map((habit) => (
+            <Habit
+              key={habit.id}
+              habit={habit}
+              onIncrement={this.props.onIncrement}
+              onDecrement={this.props.onDecrement}
+              onDelete={this.props.onDelete}
+            />
+          ))}
+        </ul>
+        <button className="habits-reset" onClick={this.props.onReset}>
+          Reset All
+        </button>
+      </>
     );
   }
 }
