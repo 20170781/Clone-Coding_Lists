ea51e46cf3fd8211196ec6ef13faf52a048afd7b
20170781 <lkh981107@naver.com>
2021-07-14 10:48:04 +0900

    show video list with title

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    console.log("effect");
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD7iyS8SUQk4JcBtAu6GBHdTasB93dBbxI",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
   }, []);
-  return <div className="App"></div>;
+  return <VideoList videos={videos} />;
 }
 
 export default App;
