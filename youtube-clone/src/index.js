8e1f2939a0c2b9d904e99d426d49658fb83a8289
20170781 <lkh981107@naver.com>
2021-07-15 01:19:40 +0900

    update youtube

--- youtube-clone/src/index.js
+++ youtube-clone/src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./app.jsx";
 import Youtube from "./service/youtube";
-const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
+import axios from "axios";
+
+const httpClient = axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
+});
+
+const youtube = new Youtube(httpClient);
 
 ReactDOM.render(
   <React.StrictMode>
