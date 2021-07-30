1a0f300f8d459dc915ee558a3c700b08fd2927bf
20170781 <lkh981107@naver.com>
2021-07-30 10:35:44 +0900

    Add Prettier & ESLint and Fix it

---
+++ typechain(typescript)/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  extends: [
+    'airbnb-base',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 0,
+    'linebreak-style': 0,
+    'import/extensions': 0,
+    'react/jsx-filename-extension': 0,
+    'no-unused-vars': 1,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
